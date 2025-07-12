dbd85033af30111a149760486bb7ba9f59a91971
crueter <crueter@eden-emu.dev>
2025-07-12 18:05:29 -0400

    webengine and multimedia handling
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- linux/get-dependencies.sh
+++ linux/get-dependencies.sh
@@ -36,10 +36,10 @@ pacman -Syu --noconfirm \
 		nlohmann-json \
 		patchelf \
 		python-pip \
+		qt6-5compat \
 		qt6-multimedia \
 		qt6-tools \
 		qt6-wayland \
-		qt6-webengine \
 		sdl2 \
 		spirv-headers \
 		strace \
